238b81e418572198c668339d3ae5d1e3f456f587
Chen Sheng <shen535@usc.edu>
2019-04-15 02:28:13 -0700

    optimize

--- src/classes/Server.java
+++ src/classes/Server.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 import javafx.util.Pair;
@@ -16,13 +17,14 @@ public class Server
 	public static final int SIZE = 15;
 	public static int acrossSize;
 	public static int downSize;
+	public static int totalSize;
 	public static ArrayList<Integer> acrossNumber;
 	public static ArrayList<Integer> downNumber;
 	public static ArrayList<String> acrossAnswer;
 	public static ArrayList<String> downAnswer;
 	public static ArrayList<String> acrossQuestion;
 	public static ArrayList<String> downQuestion;
-	public static ArrayList<Pair<String, Boolean>> answers;
+	public static Answer[] answers;
 	
 //	public Server(int acrossSize, int downSize, ArrayList<Integer> acrossNumber, ArrayList<Integer> downNumber,
 //			ArrayList<String> acrossAnswer, ArrayList<String> downAnswer, ArrayList<String> acrossQuestion,
@@ -39,52 +41,51 @@ public class Server
 //	}
 	public boolean check()
 	{
-		for(int i = 0;i < SIZE;++i)
+		for (int i = 0; i < SIZE; ++i)
 		{
-			for(int j = 0;j < SIZE;++j)
+			for (int j = 0; j < SIZE; ++j)
 			{
 				int size = 0;
-				if(grids[i][j].index1>0)
+				if (grids[i][j].index1 > 0)
 				{
 					++size;
 				}
-				if(grids[i][j].index2>0)
+				if (grids[i][j].index2 > 0)
 				{
 					++size;
 				}
-				if(size ==1)
+				if (size == 1)
 				{
-					int index = Math.max(grids[i][j].index1,grids[i][j].index2);
-					boolean across = Server.answers.get(index).getValue();
-					int length = Server.answers.get(index).getKey().length();
+					int index = Math.max(grids[i][j].index1, grids[i][j].index2);
+					boolean across = Server.answers[index].second;
+					int length = Server.answers[index].first.length();
 					boolean joint = false;
-					if(across)
+					if (across)
 					{
-						for(int k = 0;k < length;++k)
+						for (int k = 0; k < length; ++k)
 						{
-							if(grids[i+k][j].occurr==2)
+							if (grids[i + k][j].occurr == 2)
 							{
 								joint = true;
 								break;
 							}
 						}
-						if(joint == false)
+						if (joint == false)
 						{
 							return false;
 						}
-					}
-					else
+					} else
 					{
-
-						for(int k = 0;k < length;++k)
+						
+						for (int k = 0; k < length; ++k)
 						{
-							if(grids[i][j+k].occurr==2)
+							if (grids[i][j + k].occurr == 2)
 							{
 								joint = true;
 								break;
 							}
 						}
-						if(joint == false)
+						if (joint == false)
 						{
 							return false;
 						}
@@ -97,24 +98,24 @@ public class Server
 	
 	public boolean put(int index, int x, int y)
 	{
-		String str = Server.answers.get(index).getKey();
+		String str = answers[index].first;
 		int length = str.length();
-		boolean across = Server.answers.get(index).getValue();
-		
-		if (across)
+		if (answers[index].second)
 		{
 			if (x != 0 && grids[x - 1][y].letter != 0)
 			{
 				return false;
 			}
-			if (x+length != SIZE && grids[x + length][y].letter != 0)
+			if (x + length != SIZE && grids[x + length][y].letter != 0)
 			{
 				return false;
 			}
 			boolean joint = false;
 			for (int i = 0; i < length; ++i)
 			{
-				if (!(grids[x + i][y].letter == 0 || grids[x + i][y].letter == str.charAt(i)))
+				Grid g = grids[x + i][y];
+				char c = g.letter;
+				if (c != 0 && c != str.charAt(i))
 				{
 					return false;
 				}
@@ -124,7 +125,7 @@ public class Server
 					{
 						return false;
 					}
-					if (grids[x + i][y - 1].down == true&&grids[x + i][y].down!=true)
+					if (grids[x + i][y - 1].down == true && g.down != true)
 					{
 						return false;
 					}
@@ -135,17 +136,17 @@ public class Server
 					{
 						return false;
 					}
-					if (grids[x + i][y + 1].down == true&&grids[x + i][y].down!=true)
+					if (grids[x + i][y + 1].down == true && g.down != true)
 					{
 						return false;
 					}
 				}
-				if (grids[x + i][y].letter == str.charAt(i))
+				if (c == str.charAt(i))
 				{
 					joint = true;
 				}
 			}
-			if (index == Server.answers.size() - 1 && joint == false)
+			if (index == totalSize - 1 && joint == false)
 			{
 				return false;
 			}
@@ -176,57 +177,59 @@ public class Server
 			
 		} else
 		{
-			if (y != 0 && grids[x][y-1].letter != 0)
+			if (y != 0 && grids[x][y - 1].letter != 0)
 			{
 				return false;
 			}
-			if (y+length != SIZE && grids[x][y+length].letter != 0)
+			if (y + length != SIZE && grids[x][y + length].letter != 0)
 			{
 				return false;
 			}
 			boolean joint = false;
 			for (int i = 0; i < length; ++i)
 			{
-				if (!(grids[x][y+i].letter == 0 || grids[x][y+1].letter == str.charAt(i)))
+				Grid g = grids[x][y + i];
+				char c = g.letter;
+				if (c != 0 && c != str.charAt(i))
 				{
 					return false;
 				}
 				if (x != 0)
 				{
-					if (grids[x-1][y+i].down == true)
+					if (grids[x - 1][y + i].down == true)
 					{
 						return false;
 					}
-					if (grids[x -1][y +i].across == true&&grids[x][y+i].across!=true)
+					if (grids[x - 1][y + i].across == true && g.across != true)
 					{
 						return false;
 					}
 				}
 				if (x != SIZE - 1)
 				{
-					if (grids[x+1][y+i].down == true)
+					if (grids[x + 1][y + i].down == true)
 					{
 						return false;
 					}
-					if (grids[x+1][y +i].across == true&&grids[x][y+i].across!=true)
+					if (grids[x + 1][y + i].across == true && g.across != true)
 					{
 						return false;
 					}
 				}
-				if (grids[x][y+i].letter == str.charAt(i))
+				if (c == str.charAt(i))
 				{
 					joint = true;
 				}
 			}
-			if (index == Server.answers.size() - 1 && joint == false)
+			if (index == totalSize - 1 && joint == false)
 			{
 				return false;
 			}
 			for (int i = 0; i < length; ++i)
 			{
-				if (grids[x][y+i].letter == 0)
+				if (grids[x][y + i].letter == 0)
 				{
-					grids[x][y+i].letter = str.charAt(i);
+					grids[x][y + i].letter = str.charAt(i);
 				}
 //					for(int a = 0;a < SIZE;++a)
 //					{
@@ -243,26 +246,27 @@ public class Server
 //						}
 //						System.out.println();
 //					}
-				grids[x][y+i].down = true;
-				grids[x][y+i].occurr += 1;
+				grids[x][y + i].down = true;
+				grids[x][y + i].occurr += 1;
 			}
 		}
-		if(grids[x][y].index1<0)
+		if (grids[x][y].index1 < 0)
 		{
 			grids[x][y].index1 = index;
-		}else
+		} else
 		{
 			grids[x][y].index2 = index;
 		}
 		return true;
 		
 	}
+	
 	public void remove(int index, int x, int y)
 	{
-		String str = Server.answers.get(index).getKey();
+		String str = Server.answers[index].first;
 		int length = str.length();
-		boolean across = Server.answers.get(index).getValue();
-		if(across)
+		boolean across = Server.answers[index].second;
+		if (across)
 		{
 			for (int i = 0; i < length; ++i)
 			{
@@ -273,30 +277,29 @@ public class Server
 				}
 				grids[x + i][y].occurr -= 1;
 			}
-		}
-		else
+		} else
 		{
 			for (int i = 0; i < length; ++i)
 			{
-				grids[x][y+i].down = false;
-				if (grids[x][y+i].occurr != 2)
+				grids[x][y + i].down = false;
+				if (grids[x][y + i].occurr != 2)
 				{
-					grids[x][y+i].letter = 0;
+					grids[x][y + i].letter = 0;
 				}
-				grids[x][y+i].occurr -= 1;
+				grids[x][y + i].occurr -= 1;
 			}
 		}
-
-			if(grids[x][y].index1==index)
-			{
-				grids[x][y].index1= -1;
-			}
-			else
-			{
-				grids[x][y].index2 = -1;
-			}
+		
+		if (grids[x][y].index1 == index)
+		{
+			grids[x][y].index1 = -1;
+		} else
+		{
+			grids[x][y].index2 = -1;
+		}
 		
 	}
+	
 	public static void main(String[] args)
 	{
 		grids = new Grid[SIZE][SIZE];
@@ -313,7 +316,7 @@ public class Server
 		Server.downAnswer = new ArrayList<String>();
 		Server.acrossQuestion = new ArrayList<String>();
 		Server.downQuestion = new ArrayList<String>();
-		Server.answers = new ArrayList<Pair<String, Boolean>>();
+		
 		String path = "gamedata";
 		int fileCount = 0;
 		File d = new File(path);
@@ -690,6 +693,7 @@ public class Server
 		}
 		Server.acrossSize = acrossNumber.size();
 		Server.downSize = downNumber.size();
+		Server.totalSize = acrossSize+downSize;
 		for (int i = 0; i < acrossSize; ++i)
 		{
 			System.out.println(acrossNumber.get(i) + "|" + acrossAnswer.get(i) + "|" + acrossQuestion.get(i));
@@ -768,75 +772,80 @@ public class Server
 		{
 			System.out.println(downNumber.get(i) + "|" + downAnswer.get(i) + "|" + downQuestion.get(i));
 		}
+		Server.answers = new Answer[totalSize];
 		int i = 0;
 		int j = 0;
+		int count = 0;
 		while (!((i == acrossSize) && (j == downSize)))
 		{
 			if (i == acrossSize)
 			{
-				answers.add(new Pair<String,Boolean>(downAnswer.get(j), false));
+				answers[count] = (new Answer(downAnswer.get(j), false));
 				++j;
 			} else if (j == downSize)
 			{
-				answers.add(new Pair<String,Boolean>(acrossAnswer.get(i), true));
+				answers[count] = (new Answer(acrossAnswer.get(i), true));
 				++i;
 			} else if (acrossAnswer.get(i).length() >= downAnswer.get(j).length())
 			{
-				answers.add(new Pair<String,Boolean>(acrossAnswer.get(i), true));
+				answers[count] = (new Answer(acrossAnswer.get(i), true));
 				++i;
 			} else
 			{
-				answers.add(new Pair<String,Boolean>(downAnswer.get(j), false));
+				answers[count] = (new Answer(downAnswer.get(j), false));
 				++j;
 			}
+			++count;
 		}
-		for (i = 0; i < answers.size(); ++i)
-		{
-			System.out.println(answers.get(i));
-		}
+//		for (i = 0; i < answers.size(); ++i)
+//		{
+//			System.out.println(answers.get(i));
+//		}
 		Server server = new Server();
-//		try
-//		{
+		long before = System.currentTimeMillis();
+		try
+		{
 			server.backtrack(0);
-//		}
-//		catch(Exception e)
-//		{
-//			System.out.println(e.getMessage());
-//		}
+		} catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+		}
+		long after = System.currentTimeMillis();
+		System.out.println("time used: " + (after - before));
 	}
 	
-	void backtrack(int index) /*throws Exception*/
+	void backtrack(int index) throws Exception
 	{
-		if (index == answers.size())
+		if (index == totalSize)
 		{
-			if(check())
+			if (check())
 			{
-				for(int i = 0;i < SIZE;++i)
+				for (int i = 0; i < SIZE; ++i)
 				{
-					for(int j = 0;j < SIZE;++j)
+					for (int j = 0; j < SIZE; ++j)
 					{
-						if(grids[i][j].letter==0)
+						if (grids[i][j].letter == 0)
 						{
 							System.out.print(' ');
-						}
-						else
+						} else
 						{
 							System.out.print(grids[i][j].letter);
 						}
 					}
 					System.out.println();
 				}
-				//throw new Exception("Founded!");
+				throw new Exception("Founded!");
 			}
 			return;
 		}
-		if (answers.get(index).getValue())
+		int length = answers[index].first.length();
+		if (answers[index].second)
 		{
-			for (int j = 0; j < SIZE ; ++j)
+			for (int j = 0; j < SIZE; ++j)
 			{
-				for (int i = 0; i < SIZE + 1 - (answers.get(index).getKey().length()); ++i)
+				for (int i = 0; i < SIZE + 1 - length; ++i)
 				{
-					if(put(index, i, j))
+					if (put(index, i, j))
 					{
 //						for(int x = 0;x < SIZE;++x)
 //						{
@@ -854,19 +863,18 @@ public class Server
 //							System.out.println();
 //						}
 //						System.out.println(answers.get(index).getKey()+" "+i+" "+j);
-						backtrack(index+1);
+						backtrack(index + 1);
 						remove(index, i, j);
 					}
 				}
 			}
-		}
-		else
+		} else
 		{
-			for (int j = 0; j < SIZE + 1 - (answers.get(index).getKey().length()); ++j)
+			for (int j = 0; j < SIZE + 1 - length; ++j)
 			{
 				for (int i = 0; i < SIZE; ++i)
 				{
-					if(put(index, i, j))
+					if (put(index, i, j))
 					{
 //						for(int x = 0;x < SIZE;++x)
 //						{
@@ -884,7 +892,7 @@ public class Server
 //							System.out.println();
 //						}
 //						System.out.println(answers.get(index).getKey()+" "+i+" "+j);
-						backtrack(index+1);
+						backtrack(index + 1);
 						remove(index, i, j);
 					}
 				}
@@ -901,6 +909,7 @@ class Grid
 	public boolean across;
 	public char letter;
 	public int occurr;
+	
 	public Grid()
 	{
 		index1 = -1;
@@ -912,3 +921,14 @@ class Grid
 	}
 }
 
+class Answer
+{
+	String first;
+	boolean second;
+	
+	public Answer(String first, boolean second)
+	{
+		this.first = first;
+		this.second = second;
+	}
+}
